98a2b214853aca7f2197fd03dfcbaec6e6b9059a
Gabriel Spindola <gabrielspindola@elevor.com>
2023-02-24 14:28:15 -0300

    dev

--- Meus testes/Form1.Designer.cs
+++ Meus testes/Form1.Designer.cs
@@ -138,7 +138,7 @@
             this.Col_TimeSpend});
             this.dataGridView1.Location = new System.Drawing.Point(15, 69);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1076, 318);
+            this.dataGridView1.Size = new System.Drawing.Size(1068, 318);
             this.dataGridView1.TabIndex = 16;
             // 
             // dataSet1
