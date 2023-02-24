f190f3948afc3c545c553dc9901b355565fc0153
Gabriel Spindola <gabrielspindola@elevor.com>
2023-02-24 09:57:31 -0300

    dev

--- Meus testes/Form1.Designer.cs
+++ Meus testes/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.buttonAbrirArquivo = new System.Windows.Forms.Button();
             this.labelFilePath = new System.Windows.Forms.Label();
@@ -37,22 +36,18 @@
             this.timeBox = new System.Windows.Forms.NumericUpDown();
             this.labelTime = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.radioButtonOrderByDate = new System.Windows.Forms.RadioButton();
-            this.radioButtonOrderByTimeTaken = new System.Windows.Forms.RadioButton();
             this.buttonGo = new System.Windows.Forms.Button();
-            this.labelOrderBy = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.Col_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col_Datetime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col_ServerIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Col_ClientIP = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.timeBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(12, 77);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(404, 308);
-            this.richTextBox1.TabIndex = 3;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
             // 
             // openFileDialog1
             // 
@@ -115,73 +110,83 @@
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(286, 393);
+            this.pictureBox1.Location = new System.Drawing.Point(740, 393);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(130, 31);
             this.pictureBox1.TabIndex = 11;
             this.pictureBox1.TabStop = false;
             // 
-            // radioButtonOrderByDate
-            // 
-            this.radioButtonOrderByDate.AutoSize = true;
-            this.radioButtonOrderByDate.Checked = true;
-            this.radioButtonOrderByDate.Location = new System.Drawing.Point(207, 45);
-            this.radioButtonOrderByDate.Name = "radioButtonOrderByDate";
-            this.radioButtonOrderByDate.Size = new System.Drawing.Size(48, 17);
-            this.radioButtonOrderByDate.TabIndex = 12;
-            this.radioButtonOrderByDate.TabStop = true;
-            this.radioButtonOrderByDate.Text = "Date";
-            this.radioButtonOrderByDate.UseVisualStyleBackColor = true;
-            this.radioButtonOrderByDate.CheckedChanged += new System.EventHandler(this.radioButtonOrderByDate_CheckedChanged);
-            // 
-            // radioButtonOrderByTimeTaken
-            // 
-            this.radioButtonOrderByTimeTaken.AutoSize = true;
-            this.radioButtonOrderByTimeTaken.Location = new System.Drawing.Point(253, 45);
-            this.radioButtonOrderByTimeTaken.Name = "radioButtonOrderByTimeTaken";
-            this.radioButtonOrderByTimeTaken.Size = new System.Drawing.Size(78, 17);
-            this.radioButtonOrderByTimeTaken.TabIndex = 13;
-            this.radioButtonOrderByTimeTaken.TabStop = true;
-            this.radioButtonOrderByTimeTaken.Text = "Time taken";
-            this.radioButtonOrderByTimeTaken.UseVisualStyleBackColor = true;
-            // 
             // buttonGo
             // 
             this.buttonGo.Enabled = false;
-            this.buttonGo.Location = new System.Drawing.Point(353, 42);
+            this.buttonGo.Location = new System.Drawing.Point(798, 40);
             this.buttonGo.Name = "buttonGo";
-            this.buttonGo.Size = new System.Drawing.Size(63, 23);
+            this.buttonGo.Size = new System.Drawing.Size(72, 23);
             this.buttonGo.TabIndex = 14;
             this.buttonGo.Text = "Go";
             this.buttonGo.UseVisualStyleBackColor = true;
             this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
             // 
-            // labelOrderBy
+            // dataGridView1
             // 
-            this.labelOrderBy.AutoSize = true;
-            this.labelOrderBy.Location = new System.Drawing.Point(156, 47);
-            this.labelOrderBy.Name = "labelOrderBy";
-            this.labelOrderBy.Size = new System.Drawing.Size(50, 13);
-            this.labelOrderBy.TabIndex = 15;
-            this.labelOrderBy.Text = "Order by:";
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Col_ID,
+            this.Col_Datetime,
+            this.Col_ServerIP,
+            this.Col_ClientIP});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(858, 318);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // Col_ID
+            // 
+            this.Col_ID.HeaderText = "ID";
+            this.Col_ID.Name = "Col_ID";
+            this.Col_ID.ReadOnly = true;
+            this.Col_ID.Width = 40;
+            // 
+            // Col_Datetime
+            // 
+            this.Col_Datetime.HeaderText = "Date-time";
+            this.Col_Datetime.Name = "Col_Datetime";
+            this.Col_Datetime.ReadOnly = true;
+            this.Col_Datetime.Width = 160;
+            // 
+            // Col_ServerIP
+            // 
+            this.Col_ServerIP.HeaderText = "Server IP";
+            this.Col_ServerIP.Name = "Col_ServerIP";
+            this.Col_ServerIP.ReadOnly = true;
+            // 
+            // Col_ClientIP
+            // 
+            this.Col_ClientIP.HeaderText = "Client IP";
+            this.Col_ClientIP.Name = "Col_ClientIP";
+            this.Col_ClientIP.ReadOnly = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(427, 434);
-            this.Controls.Add(this.labelOrderBy);
+            this.ClientSize = new System.Drawing.Size(882, 434);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonGo);
-            this.Controls.Add(this.radioButtonOrderByTimeTaken);
-            this.Controls.Add(this.radioButtonOrderByDate);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelTime);
             this.Controls.Add(this.timeBox);
             this.Controls.Add(this.labelCount);
             this.Controls.Add(this.labelFilePath);
             this.Controls.Add(this.buttonAbrirArquivo);
-            this.Controls.Add(this.richTextBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
@@ -189,13 +194,14 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.timeBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button buttonAbrirArquivo;
         private System.Windows.Forms.Label labelFilePath;
@@ -203,10 +209,13 @@
         private System.Windows.Forms.NumericUpDown timeBox;
         private System.Windows.Forms.Label labelTime;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.RadioButton radioButtonOrderByDate;
-        private System.Windows.Forms.RadioButton radioButtonOrderByTimeTaken;
         private System.Windows.Forms.Button buttonGo;
-        private System.Windows.Forms.Label labelOrderBy;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_Datetime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_ServerIP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Col_ClientIP;
     }
 }
 
--- Meus testes/Form1.cs
+++ Meus testes/Form1.cs
@@ -1,12 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Data;
-using System.Drawing;
-
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Globalization;
@@ -14,16 +7,11 @@ using System.Globalization;
 
 namespace Meus_testes
 {
-
     public partial class Form1 : Form
     {
         string filePath;
         int count;
         DateTime logData;
-        List<string> a = new List<string>();
-        //string[] a;
-        IDictionary<int, List<string>> dictLog = new Dictionary<int, List<string>>();
-        //IDictionary<int, Tuple<string, string, string, string, string, string, string, float>> dictLog = new Dictionary<int, Tuple<string, string, string, string, string, string, string, float>>();
 
         public Form1()
         {
@@ -36,7 +24,7 @@ namespace Meus_testes
         {
             
             count = 0;
-            richTextBox1.Text = "";
+      
 
             foreach (string line in File.ReadLines(filePath))
             {
@@ -52,8 +40,12 @@ namespace Meus_testes
                     {
                         count++;
 
-                        if (radioButtonOrderByDate.Checked == true)
-                        {
+                    
+                        
+                            string datetime = lineSplited[0] + " - " + lineSplited[1];
+                            dataGridView1.Rows.Add(count.ToString(), datetime);
+
+                            /*
                             richTextBox1.Text += "#" + count + "\n";
                             richTextBox1.Text += lineSplited[0] + " - " + lineSplited[1] + " (GWT)\n";
                             richTextBox1.Text += "server ip: " + lineSplited[2] + "\n";
@@ -61,36 +53,15 @@ namespace Meus_testes
                             richTextBox1.Text += "port: " + lineSplited[6] + "\n";
                             richTextBox1.Text += lineSplited[3] + ": " + lineSplited[4] + "\n";
                             richTextBox1.Text += "time taken: " + timeTakenLineSplited + " seconds \n\n";
-                        }
-                        else if (radioButtonOrderByTimeTaken.Checked == true)
-                        {
-                            a.Clear();
-                            a.Add(lineSplited[0]);
-                            a.Add(lineSplited[1]);
-                            a.Add(lineSplited[2]);
-                            a.Add(lineSplited[8]);
-                            a.Add(lineSplited[6]);
-                            a.Add(lineSplited[3]);
-                            a.Add(lineSplited[4]);
-                            a.Add(lineSplited[14]);
-
-                            dictLog.Add(count, a);
-
-                            //dictLog.Add(count, new Tuple<string, string, string, string, string, string, string, float>(lineSplited[0], lineSplited[1], lineSplited[2], lineSplited[8], lineSplited[6], lineSplited[3], lineSplited[4], timeTakenLineSplited));
-                        }
+                            */
+                        
+                       
                     }
                 }
             }
 
             if (count > 0)
             {
-                if (radioButtonOrderByTimeTaken.Checked == true)
-                {
-                    var sortedDict = from entry in dictLog orderby entry.Value ascending select entry;
-                    foreach (var value in sortedDict) richTextBox1.Text += value + "\n";
-                
-                }
-
                 labelCount.Visible = true;
                 labelCount.Text = "Total: " + count.ToString() + " logs";
             }
@@ -118,7 +89,6 @@ namespace Meus_testes
 
                 if (openFileDialog.ShowDialog() == DialogResult.OK)
                 {
-                    richTextBox1.Text = "";
                     labelFilePath.Visible = true;
                     labelFilePath.Text = openFileDialog.FileName;
                     filePath = openFileDialog.FileName;
@@ -163,136 +133,3 @@ namespace Meus_testes
         }
     }
 }
-
-/*
- * 
- * using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.IO;
-using System.Globalization;
-
-
-namespace Meus_testes
-{
-
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-
-            InitializeComponent();
-
-        }
-
-        private void ReadLog()
-        {                    
-            using (OpenFileDialog openFileDialog = new OpenFileDialog())
-            {
-                openFileDialog.InitialDirectory = @"c:\";
-                openFileDialog.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
-                openFileDialog.FilterIndex = 2;
-                openFileDialog.RestoreDirectory = true;
-
-                if (openFileDialog.ShowDialog() == DialogResult.OK)
-                {
-                    richTextBox1.Text = "";              
-                    labelFilePath.Visible = true;
-                    labelFilePath.Text = openFileDialog.FileName;
- 
-                    var readedLines = File.ReadAllLines(openFileDialog.FileName);
-
-                    int count = 0;
-
-                    foreach (var line in readedLines)
-                    {   
-                        string[] lineSplited = line.Split(' ');   
-                        DateTime logData;
-
-                        if (DateTime.TryParse(lineSplited[0], out logData))
-                        {
-                            float timeTakenLineSplited = float.Parse(lineSplited[14], CultureInfo.InvariantCulture.NumberFormat);
-                            
-                            timeTakenLineSplited /= 1000;
-                     
-                            if((float)timeBox.Value <= timeTakenLineSplited)
-                            {
-                                count++;
-
-                                richTextBox1.Text += "#" + count + "\n";
-                                richTextBox1.Text += lineSplited[0] + " - " + lineSplited[1] + " (GWT)\n";
-                                richTextBox1.Text += "server ip: " + lineSplited[2] + "\n";
-                                richTextBox1.Text += "client ip: " + lineSplited[8] + "\n";
-                                richTextBox1.Text += "port: " + lineSplited[6] + "\n";
-                                richTextBox1.Text += lineSplited[3] + ": " + lineSplited[4] + "\n";
-                                richTextBox1.Text += "time taken: " + timeTakenLineSplited + " seconds \n\n";
-                            }                       
-                        }
-                    }
-
-                    labelCount.Visible = true;
-                    labelCount.Text = "Total: " + count.ToString() + " logs";
-
-                }
-            }
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonAbrirArquivo_Click(object sender, EventArgs e)
-        {
-            if (timeBox.Value != 0) ReadLog();
-            else {
-                if (MessageBox.Show("A time set 0 will show all logs. Continue?", "Confirmation", MessageBoxButtons.OKCancel) == DialogResult.OK) ReadLog();
-            }
-            
-        }
-        
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-     
-        }
-
-        private void labelCount_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void labelTitleDoc_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void timeBox_ValueChanged(object sender, EventArgs e)
-        {
-          
-        }
-
-        private void labelTime_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButtonOrderByDate_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-    }
-}
-
- * 
- * 
- */
